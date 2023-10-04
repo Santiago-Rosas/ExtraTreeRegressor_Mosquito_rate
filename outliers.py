2acf72549f040486fc6ec34800df39555b6b3655
Santiago Rosas <santiagorosasplaza@gmail.com>
2023-10-04 16:02:51 -0600

    some corrections

--- outliers.py
+++ outliers.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-#fucntions to detect and repalce or remove out_layers 
+#fucntions to detect and repalce or remove outliers 
 
 ## extract low limit and upper limit 
 def outlier_thresholds(dataframe=pd.DataFrame, variable=str):
@@ -12,44 +12,40 @@ def outlier_thresholds(dataframe=pd.DataFrame, variable=str):
     low_limit = quartile1 - 1.5 * interquantile_range
     return low_limit.round(), up_limit.round()
 
-##suum all out layers per feature
+##suum all outliers per feature
 
-def sum_out_layers(dataframe=pd.DataFrame, variable=str):
+def sum_outliers(dataframe=pd.DataFrame, variable=str):
     low_limit, up_limit = outlier_thresholds(dataframe, variable)
     lista = [i for i in dataframe[variable] if i < low_limit or i > up_limit]
     return len(lista)
             
-    #for i in dataframe[variable]:
-     #    if i  < low_limit or i > up_limit:
-    #        lista.append(i)
-    #return len(lista) 
-    
+ 
 
-##identifie the index of the out layer 
-def index_out_layers(dataframe, variable=str):
+##identifie the index of the outliers
+def index_out_liers(dataframe, variable=str):
     index=[]
     low_limit, up_limit = outlier_thresholds(dataframe, variable)
     index.append(list(np.where((dataframe[variable] < low_limit) | (dataframe[variable] > up_limit))[0]))
     return index[0]
 
-###create a table with the number of oulayers per column 
+###create a table with the number of outliers per column 
 
-def table_outlayers(dataframe,features=list):
+def table_outliers(dataframe,features=list):
     out_layers= pd.DataFrame()
     for i in features: 
-        lista= sum_out_layers(dataframe, i)
+        lista= sum_outliers(dataframe, i)
         out_layers[i]= [lista]
         Outlayers=out_layers.T
-        Outlayers.columns=["Number_out_layer"]
-    return Outlayers.sort_values(by="Number_out_layer",ascending=False)
+        Outlayers.columns=["Number_outliers"]
+    return Outlayers.sort_values(by="Number_outliers",ascending=False)
 
 
-###remove all out layers from a df 
+###remove all outliers from a df 
 
-def remove_out_layers_from_df(dataframe, variable=list)->pd.DataFrame:
+def remove_outliers_from_df(dataframe, variable=list)->pd.DataFrame:
     all_Out_layers=[]
     for i in variable:
-        index=index_out_layers(dataframe, variable)
+        index=index_out_liers(dataframe, variable)
         for j in index:
             if j not in all_Out_layers:
                 all_Out_layers.append(j)
